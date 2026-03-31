d29e087f5a13cbd2432aa5fa9caaa64834500989
mouha-create <mouhamed-alamine.ndiaye@ism.edu.sn>
2026-03-31 23:16:53 +0000

    ton message

--- android/app/src/main/kotlin/com/tiaktiak/tiak_passenger/MainActivity.kt
+++ android/app/src/main/kotlin/com/tiaktiak/tiak_passenger/MainActivity.kt
@@ -2,4 +2,5 @@ package com.tiaktiak.tiak_passenger
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+// Legacy activity kept only to avoid stale source-path conflicts.
+class LegacyMainActivity : FlutterActivity()
